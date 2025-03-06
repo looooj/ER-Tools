14c1d7101f9e2eb13a00cb7979f52b8c7c4c823f
looooj <2553895012@qq.com>
2025-03-06 18:59:21 +0800

    Create UpdateSoul.cs
    double get soul

---
+++ ERParamUtils/UpateParam/UpdateSoul.cs
@@ -0,0 +1,51 @@
+﻿using ERParamUtils.UpdateParam;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERParamUtils.UpateParam
+{
+    public class UpdateSoul
+    {
+
+        public static void Proc(ParamProject? paramProject, UpdateCommand updateCommand) {
+
+            if (paramProject == null) {
+                return;
+            }
+
+            var param = paramProject.FindParam(ParamNames.NpcParam);
+            if (param == null)
+                return;
+
+            List<string> items = new List<string>();
+            var rows = param.Rows;
+            string key = "getSoul";
+            foreach (var row in rows)
+            {
+                if (row.Name == null)
+                    continue;
+
+
+                var v = ParamRowUtils.GetCellInt(row, key, 0);
+
+                if (v > 0)
+                {
+
+                    if (row.Name.Contains("Albinauric"))
+                    {
+                        v = v * 10;
+                    }
+                    else {
+                        v = v * 2;
+                    }
+                    updateCommand.AddItem(ParamNames.NpcParam, row.ID, key, v);
+                    
+                }
+            }
+        }
+
+    }
+}
