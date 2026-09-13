5e19fe8533af536394f77a93d940913ad27eadac
looooj <2553895012@qq.com>
2026-09-13 09:09:13 +0800

    AutoRecover New Selection

--- ERParamUtils/UpateParam/AutoRecoverConfig.cs
+++ ERParamUtils/UpateParam/AutoRecoverConfig.cs
@@ -10,15 +10,22 @@ namespace ERParamUtils.UpateParam
     public class AutoRecoverConfig
     {
 
+        const int MP1 = 1;
+        const int MP1_HP2 = 2;
+        const int MP2 = 3;
+        const int MP2_HP4 = 4;
+        
+
+
         public static string GetNames() {
 
 
-            return "none,recover mp(+2),recover hp(+4),recover hp(+4) and mp(+2)";
+            return "none,recover mp(+1),recover hp(+2) and mp(+1),recover mp(+2),recover hp(+4) and mp(+2)";
         }
 
         public static string GetValues() {
 
-            return "0,1,2,3";
+            return "0,1,2,3,4";
         }
 
         static int current=0;
@@ -28,14 +35,14 @@ namespace ERParamUtils.UpateParam
         }
 
         public static bool RecoverMp() { 
-            if ( current == 1 || current == 3 )
+            if ( current > 0)
                 return true;
             return false;
         }
 
         public static bool RecoverHp()
         {
-            if ( current == 2 || current == 3)
+            if ( current == MP1_HP2 || current == MP2_HP4)
                 return true;
             return false;
         }
@@ -44,12 +51,14 @@ namespace ERParamUtils.UpateParam
         public static string GetSpEffectKeyValues(int v) {
 
             switch (v) {
-                case 1:
+                case MP2:
                     return "changeMpPoint;-2";
-                case 2:
-                    return "changeHpPoint;-4";
-                case 3:
+                case MP2_HP4:
                     return "changeHpPoint;-4;changeMpPoint;-2";
+                case MP1:
+                    return "changeMpPoint;-1";
+                case MP1_HP2:
+                    return "changeHpPoint;-2;changeMpPoint;-1";
             }
             return "";
         }
